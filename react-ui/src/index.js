2b08b5e4430fe6998f73e19a42ebef58baf17dbf
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-13 22:12:25 -0400

    dfew

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import './index.css';
-import LoginForm from "./loginForm.jsx";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
-import LoginPage from "./LoginPage.jsx";
+
 /* import BrowserRouter from 'react-router-dom' */
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
