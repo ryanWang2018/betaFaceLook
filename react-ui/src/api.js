6544d01e87f5be5c1dcc2b7b7f141530dbb19f3b
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-14 17:28:27 -0400

    fwfew

--- react-ui/src/api.js
+++ react-ui/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://hidden-cliffs-49484.herokuapp.com/",
   timeout: 10000,
 
   withCredentials: true
