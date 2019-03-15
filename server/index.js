ee4ead858e3da4fd0249d72e1d2f865e8ad2d589
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-15 00:17:11 -0400

    fdsfsd

--- server/index.js
+++ server/index.js
@@ -68,7 +68,8 @@ if (!isDev && cluster.isMaster) {
     var allowedOrigins = [
       "https://hidden-cliffs-49484.herokuapp.com",
       "http://localhost:3001",
-      "http://127.0.0.1:5000",
+      "http://localhost:5000",
+      "http://localhost:3000",
       "http://localhost:4000"
     ];
     var origin = req.headers.origin;
