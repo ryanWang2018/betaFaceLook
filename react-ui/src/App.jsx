eeafafe9a713c979df9311969c14fc37f7c07b84
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-22 20:25:11 -0400

    add more feature to deploy

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,43 +1,45 @@
 import React, { Component } from "react";
-import LoginPage from "./loginPage";
-import RegisterForm from "./register";
+import LoginPage from "./loginPage.jsx";
+import RegisterForm from "./register.jsx";
 import GameRooms from "./gameRooms.jsx";
-import LoginForm from "./loginForm";
+import LoginForm from "./loginForm.jsx";
 import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 import api from "./api";
-import GameBoard from './gameBoard.jsx';
-import PrepareRoom from "./prepareRoom";
+import GameBoard from "./gameBoard.jsx";
+import PrepareRoom from "./prepareRoom.jsx";
 
-const checkAuth = (() => {
-  let res =
-    api.get('/user')
-      .then(res => {
-        console.log(res);
-        return true;
-      })
-      .catch(err => {
-        console.log(err);
-        return false;
-      })
-    ;
+const checkAuth = () => {
+  let res = api
+    .get("/user")
+    .then(res => {
+      console.log(res);
+      return true;
+    })
+    .catch(err => {
+      console.log(err);
+      return false;
+    });
   return res;
-
-})
+};
 const AuthRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    checkAuth()) ? (<Component {...props} />
-    ) : (<Redirect to={{
-      pathname: '/'
-    }} />
-    )
-  } />
-)
+  <Route
+    {...rest}
+    render={props =>
+      checkAuth() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/"
+          }}
+        />
+      )
+    }
+  />
+);
 
 class App extends Component {
-
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   render() {
     return (
